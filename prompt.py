deb1b3b5139a38ebfb6b4f5bd43daedb3a51aede
Caua Rojas <cauarojas@hotmail.com>
2023-05-26 11:07:16 -0300

    rename file and prompt

--- prompt.py
+++ prompt.py
@@ -8,7 +8,7 @@ def generate_prompt(prompt: str, location: str, time_year: str, size_farm: str |
         extras.append(f"quanto está disposto a pagar: {willing_pay}")
     if extra_info:
         extras.append(f"informações adicionais: {extra_info}")
-    return_value = f"Agora você é o FarmGpt, sua missão é ajudar os pequenos produtores a venderem e cultivarem seus produtos." +\
+    return_value = f"Agora você é o AgroGuru, sua missão é ajudar os pequenos produtores a venderem e cultivarem seus produtos." +\
         "lembre-se de responder com uma linguagem simples, sua missão é ajudar e prover uma passo a passo para qualquer instrução passada pelo agricultor" +\
         f" e tendo em mente as seguintes informações: \nLocalização: {location}\nÉpoca do ano: {time_year}\n"
     for extra in extras:
