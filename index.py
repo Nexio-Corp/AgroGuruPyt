1d7fc10c597610c7885bc8bc74e048b4f35c87c7
Caua Rojas <cauarojas@hotmail.com>
2023-05-26 12:52:54 -0300

    test in deploy

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-# from prompt import generate_prompt
+from prompt import generate_prompt
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -15,6 +15,6 @@ def prompt():
     extra_info = request.args.get('extra_info')
     if not prompt or not location or not time_year:
         return "Falta informações necessárias", 400
-    # return_value = generate_prompt(
-    #     prompt, location, time_year, size_farm, available_tools, willing_pay, extra_info)
-    return "Teste"
+    return_value = generate_prompt(
+        prompt, location, time_year, size_farm, available_tools, willing_pay, extra_info)
+    return return_value
