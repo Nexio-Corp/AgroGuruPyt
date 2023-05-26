35ef941687e61a06d2fe0feb88e0def6aa876eed
Caua Rojas <cauarojas@hotmail.com>
2023-05-26 11:59:16 -0300

    changed route

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/prompt', methods=['GET'])
+@app.route('/', methods=['GET'])
 def prompt():
     prompt = request.args.get('prompt')
     location = request.args.get('location')
